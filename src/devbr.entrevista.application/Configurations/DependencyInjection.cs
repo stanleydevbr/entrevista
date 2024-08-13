d95c97443b224a82b1eb8d8aa29fb2e632382901
Stanley Dias Paulo <stanleydevbr@gmail.com>
2024-08-13 09:30:13 -0300

    feat: atualizacao #1

---
+++ src/devbr.entrevista.application/Configurations/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DevBr.Entrevista.Application.Configurations
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection ApplicationRegister(this IServiceCollection services)
+        {
+            services.AddTransient<ICandidatoService, CandidatoService>();
+
+            return services;
+        }
+    }
+}
---
+++ src/devbr.entrevista.infra/Configurations/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using DevBr.Entrevista.Domain.Interfaces.Repositories;
+using DevBr.Entrevista.Infra.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevBr.Entrevista.Infra.Configurations
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection InfraRegister(this IServiceCollection services)
+        {
+            services.AddTransient<ICandidatoRepository, CandidatoRepository>();
+
+            return services;
+        }
+
+    }
+}
